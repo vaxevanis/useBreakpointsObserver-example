72dedfe9ca3fa17ecbd7beeb99d4e118f7b163fb
paulv <vaxevanis.paul@gmail.com>
2022-09-29 18:30:22 +0200

    feat: create useBreakpointsObserver example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
+import React, { createContext, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { AppContextProps } from './interface';
+import { useBreakpointsObserver } from 'use-breakpoint-observer';
+import { BreakSizesType } from 'use-breakpoint-observer/lib/types';
+
+// BreakPoint Sizes
+// This Object defines the minimum widths for specific breakpoint names.
+const breakPointSizes: BreakSizesType = {
+  xs: 480,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+  xxl: 1600,
+};
+
+// Optionally: We could pass on Context Provider our logic for what we consider to be mobile screen size
+const mobileBreakPoint = breakPointSizes.md ? breakPointSizes.md : 768;
+const appContextPropsInit: AppContextProps = {
+  isMobile: window.screen.width < mobileBreakPoint,
+};
+
+// Create the context Provider and pass the initial values
+export const AppContext = createContext<AppContextProps>(appContextPropsInit);
+
+export const App: React.FC<{}> = () => {
+  let observeRef = useRef(null);
+  // Set as null to observe the document size
+  const [breakSize, width] = useBreakpointsObserver(
+    observeRef,
+    breakPointSizes,
   );
-}
+
+  const isMobile = breakSize
+    ? breakSize === 'sm' || breakSize === 'xs'
+    : appContextPropsInit.isMobile;
+
+  return (
+    <AppContext.Provider
+      value={{
+        isMobile: isMobile,
+      }}
+    >
+      <div
+        className={`App ${breakSize} ${isMobile && 'isMobile'}`}
+        ref={observeRef}
+      >
+        <header className={`App-header`}>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h3>useBreaksizeObserver Example</h3>
+        </header>
+        <div className="App-content">
+          <p>Resize the window</p>
+          <div className="card">
+            [{isMobile ? 'Mobile' : 'Desktop'} View]
+            <p>
+              Breakpoint is: <strong>{breakSize}</strong>
+            </p>
+            <p>
+              Width is: <strong>{width?.toFixed()}px</strong>
+            </p>
+          </div>
+        </div>
+      </div>
+    </AppContext.Provider>
+  );
+};
 
 export default App;
